5ac478f8112f8c211c2540073139b2cda29c934b
Aurelien CHIREN <aurelien.chiren@gmail.com>
2021-05-30 23:49:21 +0200

    Init navigation

--- navigation/Screens.tsx
+++ navigation/Screens.tsx
@@ -10,18 +10,17 @@
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
 import React from 'react';
-
-import Screens from './navigation/Screens';
 
 const Stack = createStackNavigator();
 
-const App: React.FunctionComponent = () => {
+const Screens: React.FunctionComponent = () => {
   return (
-    <NavigationContainer>
-      <Screens />
-    </NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
   );
 };
 
-export default App;
+export default Screens;
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const HomeScreen: React.FunctionComponent = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
